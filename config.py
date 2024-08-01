a6ce80a591eebe57147dfac065d3eae9506abbf5
Hesam Mahmoodi <hesammahmoodiofficial@gmail.com>
2024-08-01 23:44:45 +0330

    add cookies:   config.py

--- config.py
+++ config.py
@@ -128,7 +128,9 @@ def panel_url_validator(url):
         url = url.replace("/admin/user", "")
     print(colored("Checking URL...", "yellow"))
     try:
-        request = requests.get(f"{url}/admin/")
+        cookies = requests.get(url).cookies
+        url_tmp = url.split('/a4c24ea5-6d1a-4c6e-8dfd-4f61ac1812d0/')[0]
+        request = requests.get(f"{url_tmp}/admin/")
     except requests.exceptions.ConnectionError as e:
         print(colored("URL is not valid! Error in connection", "red"))
         print(colored(f"Error: {e}", "red"))
